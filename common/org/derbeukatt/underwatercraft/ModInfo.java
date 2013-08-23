7aee212eebf8e70e5650409ac355aa4325986208
derBeukatt <philburghardt@googlemail.com>
2013-08-23 20:21:13 +0200

    changed fingerprint error message

--- common/org/derbeukatt/underwatercraft/ModInfo.java
+++ common/org/derbeukatt/underwatercraft/ModInfo.java
@@ -6,7 +6,7 @@ public class ModInfo {
     public static final String MOD_NAME = "UnderWaterCraft";
     public static final String MOD_VERSION = "@VERSION@ (build @BUILD_NUMBER@)";
     public static final String MOD_FINGERPRINT = "@FINGERPRINT@";
-    public static final String MOD_FINGERPRINT_MSG = "Wrong version of UWC, tinkering going on!";
+    public static final String MOD_FINGERPRINT_MSG = "Wrong version of UWC, there is definitely something wrong with your version. Redownload original version!";
 
     public static final String MOD_CHANNELS = "UnderWaterCraft";
 }
