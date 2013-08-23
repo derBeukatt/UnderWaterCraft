0b1320447589afa5b0345aa0e7b576151de42580
derBeukatt <philburghardt@googlemail.com>
2013-08-23 18:27:14 +0200

    added build process

--- common/org/derbeukatt/underwatercraft/ModInfo.java
+++ common/org/derbeukatt/underwatercraft/ModInfo.java
@@ -10,7 +10,9 @@ package org.derbeukatt.underwatercraft;
 public class ModInfo {
     public static final String MOD_ID = "UnderWaterCraft";
     public static final String MOD_NAME = "UnderWaterCraft";
-    public static final String MOD_VERSION = "0.0.1";
+    public static final String MOD_VERSION = "@VERSION@ (build @BUILD_NUMBER@)";
+    public static final String MOD_FINGERPRINT = "@FINGERPRINT@";
+    public static final String MOD_FINGERPRINT_MSG = "Wrong version of UWC, tinkering going on!";
 
     public static final String MOD_CHANNELS = "UnderWaterCraft";
 }
--- common/org/derbeukatt/underwatercraft/UnderWaterCraft.java
+++ common/org/derbeukatt/underwatercraft/UnderWaterCraft.java
@@ -1,29 +1,27 @@
 package org.derbeukatt.underwatercraft;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.derbeukatt.underwatercraft.gui.CraftingHandler;
 import org.derbeukatt.underwatercraft.items.Items;
 import org.derbeukatt.underwatercraft.network.PacketHandler;
 import org.derbeukatt.underwatercraft.proxies.CommonProxy;
 import org.derbeukatt.underwatercraft.util.ConfigHandler;
 
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLFingerprintViolationEvent;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-/**
- * Created with IntelliJ IDEA.
- * User: philipp
- * Date: 04.08.13
- * Time: 19:06
- * To change this template use File | Settings | File Templates.
- */
-@Mod(modid = ModInfo.MOD_ID, name = ModInfo.MOD_NAME, version = ModInfo.MOD_VERSION)
+@Mod(modid = ModInfo.MOD_ID, name = ModInfo.MOD_NAME, version = ModInfo.MOD_VERSION, certificateFingerprint = ModInfo.MOD_FINGERPRINT)
 @NetworkMod(channels = {ModInfo.MOD_CHANNELS}, clientSideRequired = true, serverSideRequired = false, packetHandler = PacketHandler.class)
 public class UnderWaterCraft{
 
@@ -32,7 +30,14 @@ public class UnderWaterCraft{
 
     @SidedProxy(clientSide = "org.derbeukatt.underwatercraft.proxies.ClientProxy", serverSide = "org.derbeukatt.underwatercraft.proxies.CommonProxy")
     public static CommonProxy proxy;
-
+    
+    @EventHandler
+    public void invalidFingerPrint(FMLFingerprintViolationEvent event) {
+    	Logger log = Logger.getLogger(ModInfo.MOD_ID);
+    	log.setParent(FMLLog.getLogger());
+    	log.log(Level.SEVERE, ModInfo.MOD_FINGERPRINT_MSG);
+    }
+    
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
